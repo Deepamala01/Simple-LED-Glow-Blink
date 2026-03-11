3c3299e422c02d2f1a7a53c6f29752605bb85161
DEEPAMALA <deepamala101001@gmail.com>
2026-03-11 15:31:43 +0530

    Add files via upload

---
+++ Single_LED_Glow/Simple_LED_Glow/Simple_LED_Glow.ino
@@ -0,0 +1,12 @@
+int LED1 = 2; //#define LED1 2 
+void setup() 
+{
+pinMode(LED1, OUTPUT);
+}
+
+void loop(){
+  digitalWrite(LED1,HIGH);
+  delay(1000);
+  digitalWrite(LED1,LOW);
+  delay(1000);
+}
